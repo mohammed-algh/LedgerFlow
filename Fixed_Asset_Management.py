79f7df959f16b4663cd507a12e25e64ebf9f6c67
ikema77 <brhoom.kh@gmail.com>
2023-05-19 17:05:25 +0300

    Adding & Viewing Assets

--- Fixed_Asset_Management.py
+++ Fixed_Asset_Management.py
@@ -2,19 +2,58 @@ from typing import List, Dict
 import datetime
 import pdfkit
 import sqlite3
+from PyQt5.QtWidgets import QMessageBox, QWidget
+import subprocess
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
 
+class FixedAssetManagement(QWidget):
+    def update_table(self,ui) -> None:
+        try:
+            model = QStandardItemModel()
+            model.setColumnCount(6)
+            model.setHorizontalHeaderLabels(["ID", "Name", "Date", "Price", "Salvage Val", "Years"])
+            size = len(FixedAssetManagement().get_assets())
+            for row, asset in enumerate(FixedAssetManagement().get_assets()):
+                                model.appendRow([
+                                    QStandardItem(str(asset['id'])),
+                                    QStandardItem(asset['name']),
+                                    QStandardItem(str(asset['purchase_date'])),
+                                    QStandardItem(str(asset['purchase_price'])),
+                                    QStandardItem(str(asset['salvage_value'])),
+                                    QStandardItem(str(asset['life_years']))
+                                ])
 
-class FixedAssetManagement:
-
+            ui.Asset_List.setModel(model)
+            # Set the width of the columns
+            ui.Asset_List.setColumnWidth(0, 5)  # ID column width
+            ui.Asset_List.setColumnWidth(1, 84)  # Name column width
+            ui.Asset_List.setColumnWidth(2, 70)  # Date column width
+            if size <=5:
+                ui.Asset_List.setColumnWidth(3, 59)  # Price column width
+            else:
+                ui.Asset_List.setColumnWidth(3, 42)  # Price column width
+            ui.Asset_List.setColumnWidth(4, 70)  # Sal_val column width
+            ui.Asset_List.setColumnWidth(5, 30)  # Life years column width
+        except Exception as e:
+                QMessageBox.warning(self, "Invalid Refresh", "Asset table is empty.")
     def add_asset(self, name: str, purchase_price: float, salvage_value: float,
-                  life_years: int, purchase_date=str(datetime.date.today())) -> None:
+                  life_years: int, purchase_date: str) -> None:
         """Add a fixed asset."""
-        asset = {"name": name,
+        if name=="":
+            QMessageBox.warning(self, "Missing Input", "Please provide a name for the asset.")
+        elif datetime.datetime.strptime(purchase_date, "%Y-%m-%d").date() > datetime.date.today():
+            QMessageBox.warning(self, "Invalid Date", "Date cannot be in the future.")
+        elif purchase_price <= 0 or salvage_value <= 0:
+            QMessageBox.warning(self, "Invalid Amount", "price or salvage value must be greater than zero.")
+        else:
+            asset = {"name": name,
                  "purchase_date": datetime.datetime.strptime(purchase_date, '%Y-%m-%d').date(),
                  "purchase_price": purchase_price,
                  "salvage_value": salvage_value,
                  "life_years": life_years}
-        self.save_to_database(asset)
+            self.save_to_database(asset)
+            QMessageBox.information(self, "Transaction Added!", "Transaction added successfully.")
+        
 
     def get_assets(self) -> List[Dict]:
         """Return a list of all fixed assets."""
@@ -83,7 +122,7 @@ class FixedAssetManagement:
         # Load fixed assets into memory
         for row in rows:
             assets.append({"id": row[0], "name": row[1], "purchase_date": row[2], "purchase_price": row[3],
-                           "salvage_value": row[4], "life_years": row[5]})
+                           "salvage_value": row[4], "life_years": "{:.1f}".format(row[5])})
 
         conn.close()
         return assets
